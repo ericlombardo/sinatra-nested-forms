ea30abe2dd411c0712ad1c9a318b63360b2b3b9c
Eric <eric.m.lombardo@gmail.com>
2021-02-09 17:54:08 -0500

    Build basic domain model for pirate and ships
    - Create models for Pirate and Ship
    - Create routes for '/', '/new', '/pirates'
    - Create instances from user input
    - Render and respond with new and pirates .erb files

--- app.rb
+++ app.rb
@@ -3,7 +3,27 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    # set :views => "views/pirates"
+    get '/' do 
+      erb :root
+    end
+    
+    get '/new' do # display form
+      erb :'/pirates/new'
+    end
 
+    post '/pirates' do
+      # create instance of pirate with params and assign to @pirate variable
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      
+      # loops through ships, creates new instance with user input
+      params[:pirate][:ships].each do |s|
+        Ship.new(s[:name], s[:type], s[:booty])
+      end
+      
+      @ships = Ship.all   # assigns all ships in class to @ships variable
+
+      erb :'pirates/show'   # sends to pirates/show.erb for rendering
+    end
   end
 end
